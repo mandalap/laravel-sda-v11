eb6c12f6cf6245b7191f09d67519e401822efb2b
dimasz555 <h1101211047@student.untan.ac.id>
2025-07-07 16:15:50 +0700

    add validasi konfirmasi fee

--- app/Models/FeeTransaction.php
+++ app/Models/FeeTransaction.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FeeTransaction extends Model
 {
+
+    use SoftDeletes;
+
     protected $table = 'fee_transactions';
     protected $fillable = [
         'booking_transaction_id',
